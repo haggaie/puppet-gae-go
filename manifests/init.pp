79842bbefc737cba1ff1ded53f08e957231b33d5
Haggai Eran <haggai.eran@gmail.com>
2014-06-14 11:47:03 +0300

    whitespace changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class gae-go {
 
-  package {'python2.7':
+	package {'python2.7':
 		ensure => present
 	}
 
@@ -12,18 +12,18 @@ class gae-go {
 		ensure  => present,
 	}
 
-  case $architecture {
-    'i386':  { $filename = 'go_appengine_sdk_linux_386-1.9.6.zip' }
-    'amd64': { $filename = 'go_appengine_sdk_linux_amd64-1.9.6.zip' }
-  }
+	case $architecture {
+		'i386':  { $filename = 'go_appengine_sdk_linux_386-1.9.6.zip' }
+		'amd64': { $filename = 'go_appengine_sdk_linux_amd64-1.9.6.zip' }
+	}
 
-  $url = 'https://storage.googleapis.com/appengine-sdks/featured/${filename}'
+	$url = 'https://storage.googleapis.com/appengine-sdks/featured/${filename}'
 
 	exec {"grab-gaesdk":
 		command => "/usr/bin/wget https://storage.googleapis.com/appengine-sdks/featured/${filename}",
 		cwd     => "/usr/local/src/",
 		creates => "/usr/local/src/${filename}",
-    umask   => "2",
+		umask   => "2",
 		require => Package["wget"]
 	}
 
@@ -31,7 +31,7 @@ class gae-go {
 		command => "/usr/bin/unzip ${filename} && chmod 755 go_appengine",
 		cwd     => "/usr/local/src/",
 		creates => "/usr/local/src/google_appengine",
-    umask   => "2",
+		umask   => "2",
 		require => [Exec["grab-gaesdk"], Package["unzip"]]
 	}
 }
