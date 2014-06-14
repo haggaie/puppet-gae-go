33958f7f1baee8b1a091cbb852f78a303540bdce
Haggai Eran <haggai.eran@gmail.com>
2014-06-14 11:26:16 +0300

    initial GAE Go SDK puppet module

---
+++ README.md
@@ -0,0 +1,9 @@
+# puppet-gae-go
+
+A puppet module to install [Google App Engine SDK](https://developers.google.com/appengine) for Go.
+
+```Puppet
+include gae-go
+```
+
+Tested on Ubuntu 14.04.
---
+++ manifests/init.pp
@@ -0,0 +1,37 @@
+class gae-go {
+
+  package {'python2.7':
+		ensure => present
+	}
+
+  package {'wget':
+		ensure => present
+	}
+
+	package { "unzip":
+		ensure  => present,
+	}
+
+  case $architecture {
+    'i386':  { $filename = 'go_appengine_sdk_linux_386-1.9.6.zip' }
+    'amd64': { $filename = 'go_appengine_sdk_linux_amd64-1.9.6.zip' }
+  }
+
+  $url = 'https://storage.googleapis.com/appengine-sdks/featured/${filename}'
+
+	exec {"grab-gaesdk":
+		command => "/usr/bin/wget https://storage.googleapis.com/appengine-sdks/featured/${filename}",
+		cwd     => "/usr/local/src/",
+		creates => "/usr/local/src/${filename}",
+    umask   => "2",
+		require => Package["wget"]
+	}
+
+	exec { "unzip-gae":
+		command => "/usr/bin/unzip ${filename} && chmod 755 go_appengine",
+		cwd     => "/usr/local/src/",
+		creates => "/usr/local/src/google_appengine",
+    umask   => "2",
+		require => [Exec["grab-gaesdk"], Package["unzip"]]
+	}
+}
